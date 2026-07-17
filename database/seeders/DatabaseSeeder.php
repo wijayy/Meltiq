d042e279ede579b3dff06d48b3b98aa175f6021c
wijayy <mertawijaya77@gmail.com>
2026-07-17 23:35:43 +0800

    fic icon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\InventorySeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +17,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin Meltiq',
             'email' => 'admin@meltiq.site',
+            'password' => Hash::make('EquinoxA5!')
         ]);
         User::factory()->create([
             'name' => 'User Meltiq',
             'email' => 'user@meltiq.site',
+            'password' => Hash::make('Meltiq289305')
+
         ]);
 
         $this->call([
