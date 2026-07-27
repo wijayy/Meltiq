6315c4f8d34fb33bd1406bf8cee42b0ba97421ca
wijayy <mertawijaya77@gmail.com>
2026-07-27 14:01:50 +0800

    remove primakara logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -7,16 +7,10 @@
         'grid w-full grid-cols-[3rem_1fr_3rem] items-center gap-2 rounded-lg px-1 py-1.5',
         'transition-colors ',
     ]) }}>
-    <span class="flex size-12 items-center justify-center overflow-hidden rounded-md">
-        <img class="h-full w-full object-contain p-1" src="{{ asset('assets/logo/primakara.jpg') }}"
-            alt="Logo Universitas Primakara">
-    </span>
-
-    <span class="min-w-0 text-center text-sm font-bold leading-tight text-mine-400 dark:text-mine-100">
-        {{ config('app.name', 'Meltiq CIS') }}
-    </span>
-
     <span class="flex size-12 items-center justify-center overflow-hidden rounded-md bg-mine-100">
         <x-app-logo-picture />
+    </span>
+    <span class="min-w-0 text-center text-sm font-bold leading-tight text-mine-400 dark:text-mine-100">
+        {{ config('app.name', 'Meltiq CIS') }}
     </span>
 </a>
